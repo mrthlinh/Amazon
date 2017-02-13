00a0a1b5dfa1569b859447f383c8371e6ce03112
tracymouse <mousie@live.cn>
2017-02-12 21:19:31 -0600

    foreignkey.sql
    In this branch：
    Add new table named order_items indicating the content of the order;
    Add column order_amount for Table order;
    Change shipping_state of Table address to VARCHAR(45) (to be referenced by Table saletax)
    Add foreign keys for all tables.

---
+++ SQL table/foreignkey.sql
@@ -0,0 +1,94 @@
+ALTER TABLE `yamazone`.`address` 
+CHANGE COLUMN `shipping_state` `shipping_state` VARCHAR(45) NULL DEFAULT NULL ;
+
+ALTER TABLE `yamazone`.`address` 
+ADD CONSTRAINT `address_ibfk_1`
+  FOREIGN KEY (`userID`)
+  REFERENCES `yamazone`.`customter` (`userID`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
+ALTER TABLE `yamazone`.`address` 
+ADD CONSTRAINT `address_ibfk_2`
+  FOREIGN KEY (`shipping_state`)
+  REFERENCES `yamazone`.`saletax` (`shipping_state`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
+ALTER TABLE `yamazone`.`creditcard` 
+ADD CONSTRAINT `creditcard_ibfk_1`
+  FOREIGN KEY (`userID`)
+  REFERENCES `yamazone`.`customter` (`userID`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
+ALTER TABLE `yamazone`.`evaluation` 
+ADD CONSTRAINT `evalutation_ibfk_2`
+  FOREIGN KEY (`userID`)
+  REFERENCES `yamazone`.`customter` (`userID`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
+ALTER TABLE `yamazone`.`evaluation` 
+ADD CONSTRAINT `evalutation_ibfk_1`
+  FOREIGN KEY (`productID`)
+  REFERENCES `yamazone`.`product` (`productID`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
+ALTER TABLE `yamazone`.`inventory` 
+ADD CONSTRAINT `inventory_ibfk_1`
+  FOREIGN KEY (`productID`)
+  REFERENCES `yamazone`.`product` (`productID`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
+ALTER TABLE `yamazone`.`inventory` 
+ADD CONSTRAINT `inventory_ibfk_2`
+  FOREIGN KEY (`supplierID`)
+  REFERENCES `yamazone`.`supplier` (`supplierID`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
+ALTER TABLE `yamazone`.`order` 
+ADD COLUMN `order_amount` DECIMAL(10,2) NULL AFTER `supplierID`;
+
+ALTER TABLE `yamazone`.`order` 
+ADD CONSTRAINT `order_ibfk_1`
+  FOREIGN KEY (`userID`)
+  REFERENCES `yamazone`.`customter` (`userID`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
+ALTER TABLE `yamazone`.`order` 
+ADD CONSTRAINT `order_ibfk_2`
+  FOREIGN KEY (`supplierID`)
+  REFERENCES `yamazone`.`supplier` (`supplierID`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
+CREATE TABLE `yamazone`.`order_item` (
+  `orderID` INT(11) NOT NULL,
+  `productID` INT(11) NOT NULL,
+  `product_Quantity` INT(11) NOT NULL,
+  `supplierID` INT(11) NOT NULL);
+
+ALTER TABLE `yamazone`.`order_item` ADD CONSTRAINT `order_item_ibfk_1`
+  FOREIGN KEY (`orderID`)
+  REFERENCES `yamazone`.`order` (`orderID`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
+ALTER TABLE `yamazone`.`order_item` ADD CONSTRAINT `order_item_ibfk_2`
+  FOREIGN KEY (`productID`)
+  REFERENCES `yamazone`.`product` (`productID`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
+ALTER TABLE `yamazone`.`order_item` ADD CONSTRAINT `order_item_ibfk_3`
+  FOREIGN KEY (`supplierID`)
+  REFERENCES `yamazone`.`supplier` (`supplierID`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
+
